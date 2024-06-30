e916be8c8f1da9e54ee840dbb7e08dba1fcb7b50
zedja123 <miguel.zedja@hotmail.com>
2024-06-30 04:27:32 -0300

    Update c270000004.lua

--- script/c270000004.lua
+++ script/c270000004.lua
@@ -39,7 +39,7 @@ function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_REMOVED)
 end
 
-function s.thop(e,tp,eg,ep,ev,re,r,rp)
+function s.thtop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_REMOVED,0,1,1,nil)
 	if #g>0 then
