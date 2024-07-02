8b96fc324297de47313caae827d92af191593d5c
zedja123 <miguel.zedja@hotmail.com>
2024-07-02 12:34:44 -0300

    Update c270000104.lua

--- script/c270000104.lua
+++ script/c270000104.lua
@@ -83,7 +83,7 @@ function s.tdcon(e,tp,eg,ep,ev,re,r,rp)
 end
 
 function s.banfilter(c)
-	return c:IsType(TYPE_SPELL) and c:IsAbleToRemoveAsCost() and Duel.IsExistingMatchingCard(s.tdfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,LOCATION_REMOVED,1,nil,exclude)
+	return c:IsType(TYPE_SPELL) and c:IsAbleToRemoveAsCost() and Duel.IsExistingMatchingCard(s.tdfilter,LOCATION_GRAVE+LOCATION_REMOVED,LOCATION_REMOVED,1,nil,exclude)
 end
 
 function s.tdcost(e,tp,eg,ep,ev,re,r,rp,chk)
