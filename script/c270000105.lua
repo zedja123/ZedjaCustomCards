a35a0ab1c7f30bd951f41704d8c3f0b668d5c084
zedja123 <miguel.zedja@hotmail.com>
2024-07-03 03:54:18 -0300

    Update c270000105.lua

--- script/c270000105.lua
+++ script/c270000105.lua
@@ -57,12 +57,9 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
-function s.banfilter(c,tp)
-	return (c:IsAbleToDeck() or c:IsAbleToHand()) and c:IsFaceup()
-end
 
 function s.tdfilter(c, tp)
-	return (c:IsAbleToDeck() or c:IsAbleToHand()) and c:IsFaceup() and Duel.IsExistingMatchingCard(s.banfilter,tp,LOCATION_REMOVED,LOCATION_REMOVED,1,c,tp)
+	return c:IsAbleToDeck() and c:IsAbleToHand() and c:IsFaceup()
 end
 
 function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk)
