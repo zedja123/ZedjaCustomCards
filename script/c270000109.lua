6df20751d51f5f01cbbc8691f6c9bcac67b2280d
zedja123 <miguel.zedja@hotmail.com>
2024-07-14 21:24:21 -0300

    Update c270000109.lua

--- script/c270000109.lua
+++ script/c270000109.lua
@@ -95,10 +95,10 @@ function s.detachop(e,tp,eg,ep,ev,re,r,rp)
 		-- Optional banish a Spell and inflict damage
 		if tc and Card.IsCanBeDisabledByEffect(tc,e,REASON_EFFECT)~=0 
 			and Duel.SelectMatchingCard(tp,s.banfilter,tp,LOCATION_HAND+LOCATION_ONFIELD+LOCATION_GRAVE,0,1,1,nil) then
-			Duel.SelectYesNo(tp,aux.Stringid(id,2)) 
 			local sg=Duel.SelectMatchingCard(tp,s.banfilter,tp,LOCATION_HAND+LOCATION_ONFIELD+LOCATION_GRAVE,0,1,1,nil)
 			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-			if Duel.Remove(sg,POS_FACEUP,REASON_COST)~=0 then
+			if Duel.SelectYesNo(tp,aux.Stringid(id,2)) 
+			and Duel.Remove(sg,POS_FACEUP,REASON_COST)~=0 then
 				Duel.Damage(1-tp,500,REASON_EFFECT)
 			end
 		end
