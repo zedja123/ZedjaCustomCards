b7a9b08ad2db89058b8863b840b780578e14f6aa
zedja123 <miguel.zedja@hotmail.com>
2024-07-21 15:26:32 -0300

    Update c270000106.lua

--- script/c270000106.lua
+++ script/c270000106.lua
@@ -28,49 +28,30 @@ end
 
 
 function s.filter(c,e)
-	return c:IsFaceup() and c:IsCanBeEffectTarget(e)
+return c:IsFaceup() and c:IsCanBeEffectTarget(e)
 end
-function s.xyzfilter(c,mg,tp,chk)
-	return c:IsXyzSummonable(nil,mg,2,2) and c:IsSetCard(0xf11) and (not chk or Duel.GetLocationCountFromEx(tp,tp,mg,c)>0)
+function s.xyzfilter(c,mg,tp)
+return c:IsXyzSummonable(mg,mg,1,2) and Duel.GetLocationCountFromEx(tp,tp,mg,c)>0
 end
-function s.mfilter1(c,mg,exg,tp)
-	return mg:IsExists(s.mfilter2,1,c,c,exg,tp)
-end
-function s.zonecheck(c,tp,g)
-	return Duel.GetLocationCountFromEx(tp,tp,g,c)>0 and c:IsXyzSummonable(nil,g)
-end
-function s.mfilter2(c,mc,exg,tp)
-	local g=Group.FromCards(c,mc)
-	return exg:IsExists(s.zonecheck,1,nil,tp,g)
+function s.rescon(sg,e,tp,mg)
+return Duel.IsExistingMatchingCard(s.xyzfilter,tp,LOCATION_EXTRA,0,1,nil,sg,tp)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return false end
-	local mg=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,0,nil,e)
-	local exg=Duel.GetMatchingGroup(s.xyzfilter,tp,LOCATION_EXTRA,0,nil,mg)
-	if chk==0 then return mg:IsExists(s.mfilter1,1,nil,mg,exg,tp) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
-	local sg1=mg:FilterSelect(tp,s.mfilter1,1,1,nil,mg,exg,tp)
-	local tc1=sg1:GetFirst()
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
-	local sg2=mg:FilterSelect(tp,s.mfilter2,1,1,tc1,tc1,exg,tp)
-	sg1:Merge(sg2)
-	Duel.SetTargetCard(sg1)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+if chkc then return false end
+local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,0,nil,e)
+if chk==0 then return aux.SelectUnselectGroup(g,e,tp,1,2,s.rescon,0) end
+local sg=aux.SelectUnselectGroup(g,e,tp,1,2,s.rescon,1,tp,HINTMSG_XMATERIAL,s.rescon)
+Duel.SetTargetCard(sg)
+Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
-
-function s.tfilter(c,e)
-	return c:IsRelateToEffect(e) and c:IsFaceup()
-end
-
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(s.tfilter,nil,e)
-	if #g<2 then return end
-	local xyzg=Duel.GetMatchingGroup(s.xyzfilter,tp,LOCATION_EXTRA,0,nil,g,tp,true)
-	if #xyzg>0 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local xyz=xyzg:Select(tp,1,1,nil):GetFirst()
-		Duel.XyzSummon(tp,xyz,nil,g)
-	end
+local g=Duel.GetTargetCards(e):Filter(Card.IsFaceup,nil)
+if #g<1 then return end
+local xyzg=Duel.GetMatchingGroup(s.xyzfilter,tp,LOCATION_EXTRA,0,nil,g,tp)
+if #xyzg>0 then
+Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+local xyz=xyzg:Select(tp,1,1,nil):GetFirst()
+Duel.XyzSummon(tp,xyz,nil,g)
 end
 
 function s.thfilter(c)
