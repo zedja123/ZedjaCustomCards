3970f678958a095397831f824b23dcee5ec50e73
zedja123 <miguel.zedja@hotmail.com>
2024-07-21 00:57:14 -0300

    Update c270000104.lua

--- script/c270000104.lua
+++ script/c270000104.lua
@@ -44,8 +44,12 @@ function s.filter(c)
 	return c:IsFaceup() and c:IsSetCard(0xf11) and not c:IsCode(id)
 end
 
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,nil)
+function s.spcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_MZONE,0,1,nil)
 end
 
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
