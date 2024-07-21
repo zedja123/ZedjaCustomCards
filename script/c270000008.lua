3f0c5d883fdb86b66f62f9e0cadd8d8fc2a47f74
zedja123 <miguel.zedja@hotmail.com>
2024-07-20 23:32:46 -0300

    Update c270000008.lua

--- script/c270000008.lua
+++ script/c270000008.lua
@@ -35,18 +35,6 @@ function s.spcon(e,c)
 		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_MZONE,0,1,nil)
 end
 
-function s.sptg(e, tp, eg, ep, ev, re, r, rp, chk)
-	if chk == 0 then return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0 and e:GetHandler():IsCanBeSpecialSummoned(e, 0, tp, false, false) end
-	Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, e:GetHandler(), 1, 0, 0)
-end
-
-function s.spop(e, tp, eg, ep, ev, re, r, rp)
-	local c = e:GetHandler()
-	if c:IsRelateToEffect(e) then
-		Duel.SpecialSummon(c, 0, tp, tp, false, false, POS_FACEUP)
-	end
-end
-
 function s.spfilter(c)
 	return c:IsSetCard(0xf10) and c:IsType(TYPE_MONSTER)
 end
