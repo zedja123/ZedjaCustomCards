1089ffebcbc0c4bb2a11cf3395f02bd1bd793efe
zedja123 <miguel.zedja@hotmail.com>
2024-07-25 18:49:21 -0300

    Update c271000004.lua

--- script/c271000004.lua
+++ script/c271000004.lua
@@ -26,8 +26,7 @@ function s.initial_effect(c)
 end
 
 function s.cfilter(c,tp)
-	return c:IsReason(REASON_BATTLE+REASON_EFFECT) and c:IsPreviousLocation(LOCATION_MZONE)
-		and c:IsPreviousControler(tp) and c:GetPreviousAttributeOnField()==ATTRIBUTE_FIRE
+	return c:IsReason(REASON_BATTLE+REASON_EFFECT) and c:IsPreviousControler(tp) and c:GetPreviousAttributeOnField()==ATTRIBUTE_FIRE
 end
 
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
