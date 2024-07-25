6889c7416c437effc3ed34553010284ee0fbfe00
zedja123 <miguel.zedja@hotmail.com>
2024-07-25 01:58:14 -0300

    Update c271000003.lua

--- script/c271000003.lua
+++ script/c271000003.lua
@@ -98,7 +98,7 @@ end
 -- If this card is destroyed condition
 function s.spcon(c,e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	return c:IsReason(REASON_BATTLE) or (c:IsReason(REASON_EFFECT) and c:IsSummonType(SUMMON_TYPE_SYNCHRO)
+	return c:IsReason(REASON_BATTLE) or c:IsReason(REASON_EFFECT) and c:IsSummonType(SUMMON_TYPE_SYNCHRO)
 end
 
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
