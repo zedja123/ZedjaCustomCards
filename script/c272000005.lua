9d3606bb17a84712b270fe152e1dbf0abad91a74
zedja123 <miguel.zedja@hotmail.com>
2024-07-26 18:49:39 -0300

    Update c272000005.lua

--- script/c272000005.lua
+++ script/c272000005.lua
@@ -24,7 +24,6 @@ function s.filter1(c,e,tp)
 end
 
 function s.filter2(c,e,tp,mc,rk,pg)
-	if c.rum_limit and not c.rum_limit(mc,e) then return false end
 	return c:IsType(TYPE_XYZ) and mc:IsType(TYPE_XYZ,c,SUMMON_TYPE_XYZ,tp)
 		and c:IsRank(mc:GetRank()+1) and (c:IsSetCard(0x1073) or c:IsSetCard(0x1048))  and Duel.GetLocationCountFromEx(tp,tp,mc,c)>0
 		and mc:IsCanBeXyzMaterial(c,tp) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_XYZ,tp,false,false)
