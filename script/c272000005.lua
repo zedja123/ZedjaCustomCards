b048190ed45c72ffbdc03ebc231285436292f4f6
zedja123 <miguel.zedja@hotmail.com>
2024-07-26 19:30:31 -0300

    Update c272000005.lua

--- script/c272000005.lua
+++ script/c272000005.lua
@@ -83,9 +83,9 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local sg=g:Select(tp,1,1,nil)
 	if sg:GetFirst():IsLocation(LOCATION_EXTRA) then
-		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+		Duel.SpecialSummon(sg,0,tp,1-tp,false,false,POS_FACEUP)
 	else
-		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+		Duel.SpecialSummon(sc,0,tp,1-tp,false,false,POS_FACEUP)
 	end
 	Duel.ShuffleDeck(1-tp)
 	Duel.ShuffleExtra(1-tp)
