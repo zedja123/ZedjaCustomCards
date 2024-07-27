cdd81260c5401ad39f03f54546e89bd6477928c4
zedja123 <miguel.zedja@hotmail.com>
2024-07-27 02:07:21 -0300

    Update c272000005.lua

--- script/c272000005.lua
+++ script/c272000005.lua
@@ -81,8 +81,8 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local sg=g:Select(tp,1,1,nil)
 		if sg then
-			local b1=Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.GetLocationCountFromEx(tp,tp,c)>0 
-			local b2=Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0 and Duel.GetLocationCountFromEx(1-tp,tp,c)>0
+			local b1=Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.GetLocationCountFromEx(tp,nil,sg)>0 
+			local b2=Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0 and Duel.GetLocationCountFromEx(1-tp,nil,sg)>0
 			local op=0
 		else
 			if b1 and b2 then
