611e8cbfb283428ff8cfb33519a29f5934c38629
zedja123 <miguel.zedja@hotmail.com>
2024-07-27 01:10:42 -0300

    Update c272000005.lua

--- script/c272000005.lua
+++ script/c272000005.lua
@@ -82,26 +82,9 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.ConfirmCards(tp,g)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local sg=g:Select(tp,1,1,nil)
-		if not sg:IsLinkMonster(sg) then
+		if sg then
 			local b1=Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
 			local b2=Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0
-			local op=0
-			if b1 and b2 then
-				op=Duel.SelectOption(tp,aux.Stringid(id,2),aux.Stringid(id,3))
-			elseif b1 then
-				op=Duel.SelectOption(tp,aux.Stringid(id,2))
-			elseif b2 then
-				op=Duel.SelectOption(tp,aux.Stringid(id,3))+1
-			else return end
-			if op==0 then
-				Duel.SpecialSummon(sg,0,tp,tp,true,true,POS_FACEUP)
-			else
-				Duel.SpecialSummon(sg,0,tp,1-tp,true,true,POS_FACEUP)
-			end
-		end
-		if sg:IsLinkMonster(sg) then
-			local b1=Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and e:GetHandler():GetLinkedZone(tp)
-			local b2=Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0  and e:GetHandler():GetLinkedZone(1-tp)
 			local op=0
 			if b1 and b2 then
 				op=Duel.SelectOption(tp,aux.Stringid(id,2),aux.Stringid(id,3))
