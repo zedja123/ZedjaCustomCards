e610fc31ddf521590b261a94b1ee6c22d9ca41a8
zedja123 <miguel.zedja@hotmail.com>
2024-08-05 10:11:22 -0300

    Update c270000301.lua

--- script/c270000301.lua
+++ script/c270000301.lua
@@ -102,7 +102,7 @@ end
 
 -- Monster Effects
 function s.thfilter(c)
-	return c:IsSetCard(0xf13) and not c:IsCode(id) and c:IsAbleToHand()
+	return c:IsSetCard(0xf13) and c:IsType(TYPE_MONSTER) and not c:IsCode(id) and c:IsAbleToHand()
 end
 
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
