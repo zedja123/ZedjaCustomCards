9701455a80653a266d03c5f75f4d7ef5e34f8dd9
zedja123 <miguel.zedja@hotmail.com>
2024-08-06 06:51:54 -0300

    Update c270000308.lua

--- script/c270000308.lua
+++ script/c270000308.lua
@@ -16,9 +16,9 @@ function s.initial_effect(c)
 	e2:SetCode(EVENT_CHAINING)
 	e2:SetRange(LOCATION_FZONE)
 	e2:SetCondition(s.negcon)
-	e2:SetCost(s.negcost)
+	e2:SetCost(s,negcost)
 	e2:SetTarget(s.negtg)
-	e2:SetOperation(s.negop)
+	e2:SetOperation(function(e,tp,eg,ep,ev,re,r,rp) Duel.NegateEffect(ev) end)
 	c:RegisterEffect(e2)
 end
 
@@ -40,10 +40,11 @@ function s.cfilter(c)
 	return c:IsType(TYPE_MONSTER) and (c:IsLocation(LOCATION_HAND) or c:IsFaceup())
 end
 function s.negcon(e,tp,eg,ep,ev,re,r,rp)
-	local ex,tc,loc,locp=Duel.GetChainInfo(ev,CHAININFO_TRIGGERING_EFFECT,CHAININFO_TRIGGERING_LOCATION,CHAININFO_TRIGGERING_CONTROLER)
-	return rp==1-tp and Duel.IsChainNegatable(ev) and ex
-		and (loc & (LOCATION_HAND+LOCATION_MZONE+LOCATION_SZONE+LOCATION_FZONE+LOCATION_GRAVE+LOCATION_REMOVED+LOCATION_EXTRA))~=0
-		and locp==tp and ex:GetHandler():IsSetCard(0xf13)
+	local ch=ev-1
+	if ch==0 or not (ep==1-tp and Duel.IsChainDisablable(ev)) or re:GetHandler():IsDisabled() then return false end
+	local ch_player,ch_eff=Duel.GetChainInfo(ch,CHAININFO_TRIGGERING_PLAYER,CHAININFO_TRIGGERING_EFFECT)
+	local ch_c=ch_eff:GetHandler()
+	return ch_player==tp and ((ch_c:IsSetCard(0xf13) and ch_eff:IsMonsterEffect()) and ch_eff:IsSpellTrapEffect()))
 end
 function s.negcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,nil) end
@@ -52,10 +53,6 @@ function s.negcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.Destroy(g,REASON_COST)
 end
 function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsChainNegatable(ev) end
-	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
-end
-function s.negop(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re)
-	end
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
 end
