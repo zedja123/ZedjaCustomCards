9d217b729343f9a25a488804aa06a6760e277417
zedja123 <miguel.zedja@hotmail.com>
2024-08-08 16:11:34 -0300

    LAVO

--- script/c270000304.lua
+++ script/c270000304.lua
@@ -17,7 +17,7 @@ function s.initial_effect(c)
 	--Increase ATK
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetType(EFFECT_TYPE_ACTIVATE)
+	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_PZONE)
 	e2:SetCountLimit(1)
 	e2:SetCost(s.atkcost)
