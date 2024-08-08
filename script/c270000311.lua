a45c2c7148700deb29bbd44cd9d06eaa53074893
zedja123 <miguel.zedja@hotmail.com>
2024-08-08 15:29:59 -0300

    lavo

--- script/c270000311.lua
+++ script/c270000311.lua
@@ -8,7 +8,6 @@ function s.initial_effect(c)
 	-- Place 1 "Lavoisier" Pendulum monster from your Extra Deck in your Pendulum Zone
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_TOFIELD)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
