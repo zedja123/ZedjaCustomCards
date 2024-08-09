29749d45ca0e34ce8fb1c5eddecd455f88f6870d
zedja123 <miguel.zedja@hotmail.com>
2024-08-08 21:37:03 -0300

    lavoisier

---
+++ script/c270000313.lua
@@ -0,0 +1,209 @@
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Pendulum Summon procedure
+	Pendulum.AddProcedure(c)
+	
+	-- Fusion Material
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,aux.FilterBoolFunction(Card.IsCode,270000301),aux.FilterBoolFunction(Card.IsCode,270000303),aux.FilterBoolFunction(Card.IsCode,270000304))
+
+	-- Pendulum Effect: Negate activation and Special Summon this card
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_NEGATE+CATEGORY_SPECIAL_SUMMON+CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetCountLimit(1,{id,1})
+	e1:SetCondition(s.negcon)
+	e1:SetCost(s.negcost)
+	e1:SetTarget(s.negtg)
+	e1:SetOperation(s.negop)
+	c:RegisterEffect(e1)
+	
+	-- Cannot be destroyed by battle
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
+	
+	-- Place in Pendulum Zone if leaves the field
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_LEAVE_FIELD)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e3:SetCondition(s.pencon)
+	e3:SetOperation(s.penop)
+	c:RegisterEffect(e3)
+	
+	-- Return cards to hand
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,1))
+	e4:SetCategory(CATEGORY_TOHAND+CATEGORY_DESTROY)
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetCode(EVENT_FREE_CHAIN)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1,{id,2})
+	e4:SetCost(s.retcost)
+	e4:SetTarget(s.rettg)
+	e4:SetOperation(s.retop)
+	c:RegisterEffect(e4)
+	
+	-- Negate targeting effect
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(id,2))
+	e5:SetCategory(CATEGORY_NEGATE+CATEGORY_REMOVE+CATEGORY_DESTROY)
+	e5:SetType(EFFECT_TYPE_QUICK_O)
+	e5:SetCode(EVENT_CHAINING)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetCountLimit(1,{id,3})
+	e5:SetCondition(s.negcon2)
+	e5:SetCost(s.negcost2)
+	e5:SetTarget(s.negtg2)
+	e5:SetOperation(s.negop2)
+	c:RegisterEffect(e5)
+	-- Quick Effect: Pay 3000 LP; Banish 1 monster in either GY
+	local e7=Effect.CreateEffect(c)
+	e7:SetCategory(CATEGORY_REMOVE)
+	e7:SetType(EFFECT_TYPE_QUICK_O)
+	e7:SetRange(LOCATION_MZONE)
+	e7:SetCode(EVENT_FREE_CHAIN)
+	e7:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCountLimit(1,{id,4})
+	e7:SetCost(s.cost)
+	e7:SetTarget(s.target)
+	e7:SetOperation(s.operation)
+	c:RegisterEffect(e7)
+	-- While a Link monster points to this card, that card gains the effect
+	local e8=Effect.CreateEffect(c)
+	e8:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_GRANT)
+	e8:SetRange(LOCATION_MZONE)
+	e8:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e8:SetTarget(s.eftg)
+	e8:SetLabelObject(e7)
+	c:RegisterEffect(e8)
+end
+function s.eftg(e,c)
+	return c:GetLinkedGroup():IsContains(e:GetHandler())
+end
+-- Cost function: Pay 3000 LP
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLP(tp)>3000 end
+	Duel.PayLPCost(tp,3000)
+end
+
+-- Target function: Select 1 monster in either GY
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) end
+	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
+end
+
+-- Operation function: Banish the selected monster
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
+	end
+end
+
+-- Pendulum Effect: Negate and Special Summon
+function s.negcon(e,tp,eg,ep,ev,re,r,rp)
+	return re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:IsActiveType(TYPE_SPELL+TYPE_TRAP) and Duel.IsChainNegatable(ev)
+end
+function s.negcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDestructable,tp,LOCATION_ONFIELD+LOCATION_HAND,0,2,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectMatchingCard(tp,Card.IsDestructable,tp,LOCATION_ONFIELD+LOCATION_HAND,0,2,2,nil)
+	Duel.Destroy(g,REASON_COST)
+end
+function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+end
+function s.negop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.NegateActivation(ev) and Duel.SpecialSummon(e:GetHandler(),0,tp,tp,false,false,POS_FACEUP) then
+		Duel.BreakEffect()
+	end
+end
+
+-- Place in Pendulum Zone when leaves the field
+function s.pencon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsFaceup() and c:IsPreviousLocation(LOCATION_MZONE) and Duel.CheckLocation(tp,LOCATION_PZONE,0) or Duel.CheckLocation(tp,LOCATION_PZONE,1)
+end
+function s.penop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.MoveToField(c,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
+	end
+end
+
+-- Return cards to hand
+function s.retcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDestructable,tp,LOCATION_ONFIELD,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectMatchingCard(tp,Card.IsDestructable,tp,LOCATION_ONFIELD,0,1,1,nil)
+	Duel.Destroy(g,REASON_COST)
+end
+function s.rettg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ct=s.countDifferentLavoisier(tp)
+	if chk==0 then return ct>0 and Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_ONFIELD,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,ct,0,0)
+end
+
+function s.retop(e,tp,eg,ep,ev,re,r,rp)
+	local ct=s.countDifferentLavoisier(tp)
+	if ct>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+		local g=Duel.SelectMatchingCard(tp,aux.TRUE,tp,0,LOCATION_ONFIELD,1,ct,nil)
+		if #g>0 then
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+		end
+	end
+end
+
+function s.cfilter(c)
+	return c:IsSetCard(0xf13) and c:IsType(TYPE_PENDULUM) and c:IsFaceup()
+end
+
+function s.countDifferentLavoisier(tp)
+	local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_EXTRA,0,nil)
+	local codes={}
+	local count=0
+	for tc in aux.Next(g) do
+		local code=tc:GetCode()
+		if not codes[code] then
+			codes[code]=true
+			count=count+1
+		end
+	end
+	return count
+end
+
+-- Negate targeting effect
+function s.negcon2(e,tp,eg,ep,ev,re,r,rp)
+	return re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) and Duel.IsChainNegatable(ev)
+end
+function s.negcost2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDestructable,tp,LOCATION_HAND,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectMatchingCard(tp,Card.IsDestructable,tp,LOCATION_HAND,0,1,1,nil)
+	Duel.Destroy(g,REASON_COST)
+end
+function s.negtg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_ONFIELD,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,eg,1,0,0)
+end
+function s.negop2(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.NegateActivation(ev) and Duel.Remove(eg,POS_FACEUP,REASON_EFFECT) then
+		Duel.BreakEffect()
+	end
+end
---
+++ script/c270000315.lua
@@ -0,0 +1,120 @@
+-- Define the card
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Pendulum Effect
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_PENDULUM)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetCondition(s.pendulum_condition)
+	e1:SetValue(s.pendulum_limit)
+	c:RegisterEffect(e1)
+
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCategory(CATEGORY_FUSION_SUMMON)
+	e2:SetRange(LOCATION_PZONE)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetTarget(s.pendulum_target)
+	e2:SetOperation(s.pendulum_operation)
+	c:RegisterEffect(e2)
+
+	-- Monster Effect
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCategory(CATEGORY_DESTROY+CATEGORY_TOHAND)
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e3:SetCountLimit(1,{id,2})
+	e3:SetTarget(s.monster_target)
+	e3:SetOperation(s.monster_operation)
+	c:RegisterEffect(e3)
+
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCode(EVENT_FREE_CHAIN)
+	e4:SetRange(LOCATION_PZONE)
+	e4:SetCountLimit(1,id)
+	e4:SetCost(s.pendulum_cost)
+	e4:SetTarget(s.pendulum_target)
+	e4:SetOperation(s.pendulum_operation)
+	c:RegisterEffect(e4)
+end
+
+-- Pendulum Effect: Restrict Pendulum Summons
+function s.pendulum_condition(e)
+	return e:GetHandler():IsFaceup() and e:GetHandler():IsType(TYPE_PENDULUM)
+end
+
+function s.pendulum_limit(e,c,sump,sumtype,sumpos,targetp)
+	return c:IsSetCard(0xf13) and c:IsType(TYPE_PENDULUM)
+end
+
+-- Pendulum Effect: Fusion Summon
+function s.pendulum_target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsSetCard,0xf13),tp,LOCATION_EXTRA,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_FUSION_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+
+function s.pendulum_operation(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,aux.FilterFaceupFunction(Card.IsSetCard,0xf13),tp,LOCATION_EXTRA,0,1,1,nil)
+	local tc=g:GetFirst()
+	if tc then
+		local mat=Duel.SelectFusionMaterial(tp,tc)
+		Duel.FusionSummon(tp,tc,mat)
+	end
+end
+
+-- Monster Effect: Add "Lavoisier" monsters to hand
+function s.monster_target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsSetCard,tp,LOCATION_DECK,0,2,nil,0xf13) end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,tp,LOCATION_ONFIELD)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,2,tp,LOCATION_EXTRA)
+end
+
+function s.monster_operation(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.IsExistingMatchingCard(nil,tp,LOCATION_CONTROL,0,1,nil) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+		local g=Duel.SelectMatchingCard(tp,nil,tp,LOCATION_CONTROL,0,1,1,nil)
+		if #g>0 then
+			Duel.Destroy(g,REASON_EFFECT)
+			if Duel.IsExistingMatchingCard(Card.IsSetCard,tp,LOCATION_EXTRA,0,2,nil,0xf13) then
+				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+				local hg=Duel.SelectMatchingCard(tp,Card.IsSetCard,tp,LOCATION_EXTRA,0,2,2,nil,0xf13)
+				if #hg>0 then
+					Duel.SendtoHand(hg,nil,REASON_EFFECT)
+					Duel.ConfirmCards(1-tp,hg)
+				end
+			end
+		end
+	end
+end
+
+-- Cost for placing the card in the Pendulum Zone
+function s.pendulum_cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsCanPlaceInPendulumZone(tp) end
+	-- Place the card in the Pendulum Zone as a cost
+	Duel.MoveToField(e:GetHandler(),tp,tp,LOCATION_PZONE,POS_FACEUP,true)
+end
+
+-- Target for additional Pendulum Summon
+function s.pendulum_target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
+	Duel.SetOperationInfo(0,CATEGORY_SUMMON,nil,0,tp,LOCATION_MZONE)
+end
+
+-- Operation for additional Pendulum Summon
+function s.pendulum_operation(e,tp,eg,ep,ev,re,r,rp)
+	-- Additional Pendulum Summon during Main Phase
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_EXTRA_PENDULUM_SUMMON)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,0)
+	e1:SetReset(RESET_PHASE+PHASE_MAIN1)
+	Duel.RegisterEffect(e1,tp)
+end
