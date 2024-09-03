5361233a76e920c7e7576a4817c9a4b8b7804fd1
zedja123 <miguel.zedja@hotmail.com>
2024-09-03 15:32:40 -0300

    Update c270000402.lua

--- script/c270000402.lua
+++ script/c270000402.lua
@@ -44,7 +44,7 @@ end
 
 -- Function to specify the link material count
 function s.lvval(e,c,sc)
-	return c:IsSetCard(0xf15) and c:IsType(TYPE_LINK) and Duel.GetFieldGroupCount(e:GetHandlerPlayer(),LOCATION_MZONE,0)==0
+	return c:IsType(TYPE_LINK) and Duel.GetFieldGroupCount(e:GetHandlerPlayer(),LOCATION_MZONE,0)==0
 end
 
 -- Add 1 "Build Driver" Spell/Trap to hand
