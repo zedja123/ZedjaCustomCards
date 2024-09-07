438425585653bd6a1186abe1796153597254fb1f
zedja123 <miguel.zedja@hotmail.com>
2024-09-07 18:47:57 -0300

    commit

--- script/c270000413.lua
+++ script/c270000413.lua
@@ -3,7 +3,7 @@ local s,id,o=GetID()
 function s.initial_effect(c)
 	-- Link Summon
 	c:EnableReviveLimit()
-	Link.AddProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0xf15),3,99)
+	Link.AddProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0xf15),1,99)
 	c:SetSPSummonOnce(id)
 
 	-- Also FIRE Attribute
