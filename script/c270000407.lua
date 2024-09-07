9efd3b7e314be71b507cac19dfe64f4edac63419
zedja123 <miguel.zedja@hotmail.com>
2024-09-07 16:56:49 -0300

    Update c270000407.lua

--- script/c270000407.lua
+++ script/c270000407.lua
@@ -49,7 +49,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_ADD_ATTRIBUTE)
 		e1:SetValue(att)
-		e1:SetReset(RESET_EVENT+(RESETS_STANDARD-RESET_TOFIELD)+RESET_PHASE+PHASE_END)
+		e1:SetReset(RESET_EVENT|(RESETS_STANDARD&~RESET_TOFIELD)|RESET_PHASE|PHASE_END)
 		tc:RegisterEffect(e1)
 	end
 end
