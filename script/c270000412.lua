c13622144fa55b8183001722cf70ee69f914add0
zedja123 <miguel.zedja@hotmail.com>
2024-09-09 18:39:38 -0300

    Atualizar o c270000412.lua

--- script/c270000412.lua
+++ script/c270000412.lua
@@ -56,7 +56,7 @@ end
 
 -- Material filter
 function s.matfilter(c,lc,sumtype,tp)
-	return c:IsCode(270000402) and not c:IsDisabled
+	return c:IsCode(270000402) and not c:IsDisabled()
 end
 -- Link Summon using Kiryu
 -- Custom Link Summon condition
