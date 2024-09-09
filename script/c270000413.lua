0918222a60e798f787294831d2f4af50a235df3c
zedja123 <miguel.zedja@hotmail.com>
2024-09-09 16:10:25 -0300

    Update c270000413.lua

--- script/c270000413.lua
+++ script/c270000413.lua
@@ -24,14 +24,15 @@ function s.initial_effect(c)
 	e2:SetValue(aux.tgoval)
 	c:RegisterEffect(e2)
 
-	-- Gain ATK and attack all monsters during Battle Phase
+	-- Gain ATK and attack all monsters once each during the Battle Phase
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,0))
-	e3:SetCategory(CATEGORY_ATKCHANGE)
-	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e3:SetCode(EVENT_ATTACK_ANNOUNCE)
-	e3:SetCondition(s.atkcon)
-	e3:SetOperation(s.atkop)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCondition(s.bpcon)
+	e3:SetTarget(s.bptg)
+	e3:SetOperation(s.bpoperation)
+	e3:SetCountLimit(1,{id,1}) -- Once per turn
 	c:RegisterEffect(e3)
 
 	-- Prevent opponent's activation during Battle Phase if you control another "Build Rider"
@@ -54,32 +55,42 @@ function s.initial_effect(c)
 	c:RegisterEffect(e5)
 end
 
--- Battle Phase ATK gain and attack all
-function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetCurrentPhase()==PHASE_BATTLE
+-- Condition: Only activate during the Battle Phase
+function s.bpcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsBattlePhase()
 end
-function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+
+-- Target: No specific targeting required
+function s.bptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+end
+
+-- Operation: Gain ATK and attack all monsters once each
+function s.bpoperation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
+	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil) -- Get all face-up monsters opponent controls
 	if #g>0 then
 		local atk=0
-		for tc in aux.Next(g) do
-			atk=atk+tc:GetAttack()/2
+		local tc=g:GetFirst()
+		-- Calculate total ATK gain (half the original ATK of all opponent's monsters)
+		while tc do
+			atk=atk+(tc:GetBaseAttack()/2)
+			tc=g:GetNext()
 		end
-		if c:IsRelateToBattle() and c:IsFaceup() then
+		-- Gain ATK
+		if c:IsFaceup() and c:IsRelateToEffect(e) then
 			local e1=Effect.CreateEffect(c)
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_UPDATE_ATTACK)
 			e1:SetValue(atk)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE_CAL)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 			c:RegisterEffect(e1)
 		end
-
-		-- Attack all monsters once each
+		-- Can attack all opponent's monsters once each
 		local e2=Effect.CreateEffect(c)
 		e2:SetType(EFFECT_TYPE_SINGLE)
 		e2:SetCode(EFFECT_ATTACK_ALL)
-		e2:SetValue(1)
+		e2:SetValue(1) -- Allows it to attack all opponent's monsters
 		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		c:RegisterEffect(e2)
 	end
