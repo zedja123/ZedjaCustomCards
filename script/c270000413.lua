07b7d5d82f49c6843012329b97966531bc7993eb
zedja123 <miguel.zedja@hotmail.com>
2024-09-09 16:16:08 -0300

    Update c270000413.lua

--- script/c270000413.lua
+++ script/c270000413.lua
@@ -83,7 +83,7 @@ function s.bpoperation(e,tp,eg,ep,ev,re,r,rp)
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_UPDATE_ATTACK)
 			e1:SetValue(atk)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_BATTLE)
 			c:RegisterEffect(e1)
 		end
 		-- Can attack all opponent's monsters once each
