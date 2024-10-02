1b48cebf69d123621777902899c5999d84e18478
zedja123 <miguel.zedja@hotmail.com>
2024-10-02 17:51:40 -0300

    Update c270000099.lua

--- script/c270000099.lua
+++ script/c270000099.lua
@@ -36,17 +36,12 @@ function s.initial_effect(c)
 	e3:SetOperation(s.spop2)
 	c:RegisterEffect(e3)
 
-	-- End Phase effect: send 1 Fusion Monster from Extra Deck to GY, add or Special Summon 68468459 or a monster that mentions it
+	--Register the fact it was sent to GY
 	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,0))
-	e4:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SPECIAL_SUMMON+CATEGORY_SEARCH)
-	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e4:SetCode(EVENT_PHASE+PHASE_END)
-	e4:SetRange(LOCATION_GRAVE)
-	e4:SetCondition(s.spcon2)
-	e4:SetTarget(s.sptg2)
-	e4:SetOperation(s.spop3)
-	e4:SetCountLimit(1,id)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e4:SetCode(EVENT_TO_GRAVE)
+	e4:SetOperation(s.regop)
 	c:RegisterEffect(e4)
 end
 
@@ -106,48 +101,42 @@ function s.spop2(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 -------
-
--- Condition: If this card was sent to the GY this turn
-function s.spcon2(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD+LOCATION_HAND) and e:GetHandler():IsReason(REASON_EFFECT+REASON_BATTLE)
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,2))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_PHASE+PHASE_END)
+	e1:SetRange(LOCATION_GRAVE)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+	c:RegisterEffect(e1)
 end
 
--- Target: Send 1 Fusion Monster from Extra Deck to GY and add or Special Summon 1 specific monster
-function s.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then
-		return Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_EXTRA,0,1,nil)
-			and (Duel.IsExistingMatchingCard(s.addfilter,tp,LOCATION_DECK,0,1,nil)
-			or Duel.GetLocationCount(tp,LOCATION_MZONE)>0)
-	end
-	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_EXTRA)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+function s.thfilter(c,e,tp,ft)
+	return c:IsMonster() and (c:ListsCode(CARD_ALBAZ) or c:IsCode(CARD_ALBAZ))
+		and (c:IsAbleToHand() or (c:IsCanBeSpecialSummoned(e,0,tp,false,false) and ft>0))
+end
+	--Activation legality
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil,e,tp,ft) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
 
--- Filter: Fusion Monsters in the Extra Deck that mention "Fallen of Albaz" as material
-function s.tgfilter(c)
-	return c:IsType(TYPE_FUSION) and c:ListsCode(CARD_ALBAZ) and c:IsAbleToGrave()
-end
-
--- Filter: Monsters in the Deck that either have code 68468459 or mention 68468459 in their text
-function s.addfilter(c)
-	return c:IsCode(68468459) or c:ListsCode(CARD_ALBAZ) and (c:IsAbleToHand() or c:IsCanBeSpecialSummoned(nil,0,tp,false,false))
-end
-
--- Operation: Send Fusion Monster from Extra Deck to GY, then add or Special Summon the specified monster
-function s.spop3(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	local tg=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_EXTRA,0,1,1,nil)
-	if #tg>0 and Duel.SendtoGrave(tg,REASON_EFFECT)>0 then
-		local g=Duel.SelectMatchingCard(tp,s.addfilter,tp,LOCATION_DECK,0,1,1,nil)
-		if #g>0 then
-			local opt=Duel.SelectOption(tp,aux.Stringid(id,1),aux.Stringid(id,2)) -- Option to add or Special Summon
-			if opt==0 then
-				Duel.SendtoHand(g,nil,REASON_EFFECT)
-				Duel.ConfirmCards(1-tp,g)
-			else
-				Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
-			end
-		end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	local tc=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp,ft):GetFirst()
+	if tc then
+		aux.ToHandOrElse(tc,tp,function(c)
+			return tc:IsCanBeSpecialSummoned(e,0,tp,false,false) and ft>0 end,
+		function(c)
+			Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP) end,
+		aux.Stringid(id,3))
 	end
 end
