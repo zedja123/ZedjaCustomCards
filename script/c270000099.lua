85c458939c4475dc24c90fa4e3337b599aa8d927
zedja123 <miguel.zedja@hotmail.com>
2024-10-02 18:06:31 -0300

    Update c270000099.lua

--- script/c270000099.lua
+++ script/c270000099.lua
@@ -120,17 +120,16 @@ end
 function s.scon(c)
 	if chk==0 then
 		return Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_EXTRA,0,1,nil) -- Check for matching cards
-	end
 	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_EXTRA)
 	local tg=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_EXTRA,0,1,1,nil) -- Select the matching card
 	if #tg>0 then
 		Duel.SendtoGrave(tg,REASON_EFFECT) -- Send it to the GY
+	end
 end
 
 -- Filter: Identify Fusion Monsters in Extra Deck that mention "Fallen of Albaz"
 function s.tgfilter(c)
-	return c:IsType(TYPE_FUSION) and c:ListsCode(CARD_ALBAZ)) and c:IsAbleToGrave()
-	end-- Ensure it can be sent to GY
+	return c:IsType(TYPE_FUSION) and c:ListsCode(CARD_ALBAZ)) and c:IsAbleToGrave() -- Ensure it can be sent to GY
 end
 
 function s.thfilter(c,e,tp,ft)
