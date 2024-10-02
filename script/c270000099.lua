f8cd47b842d22ff4273568f7f75b44014b6330ba
zedja123 <miguel.zedja@hotmail.com>
2024-10-02 18:21:37 -0300

    Update c270000099.lua

--- script/c270000099.lua
+++ script/c270000099.lua
@@ -110,38 +110,26 @@ function s.regop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetCode(EVENT_PHASE+PHASE_END)
 	e1:SetRange(LOCATION_GRAVE)
 	e1:SetCountLimit(1,id)
-	e1:SetCondition(s.scon)
 	e1:SetTarget(s.thtg)
 	e1:SetOperation(s.thop)
 	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 	c:RegisterEffect(e1)
 end
 
-function s.scon(c)
-	if chk==0 then
-		return Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_EXTRA,0,1,nil)
-	end-- Check for matching cards
-end
-
--- Filter: Identify Fusion Monsters in Extra Deck that mention "Fallen of Albaz"
-function s.tgfilter(c)
-	return c:IsType(TYPE_FUSION) and c:ListsCode(CARD_ALBAZ) and c:IsAbleToGrave() -- Ensure it can be sent to GY
-end
-
 function s.thfilter(c,e,tp,ft)
 	return c:IsMonster() and (c:ListsCode(CARD_ALBAZ) or c:IsCode(68468459))
 		and (c:IsAbleToHand() or (c:IsCanBeSpecialSummoned(e,0,tp,false,false) and ft>0))
 end
+function s.tgfilter(c,e,tp,ft)
+	return c:ListsCode(CARD_ALBAZ) and c:IsAbleToGraveAsCost() and c:IsType(TYPE_FUSION)
+end
 	--Activation legality
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil,e,tp,ft) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil,e,tp,ft) and Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_EXTRA)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
-	local tg=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_EXTRA,0,1,1,nil) -- Select the matching card
-	if #tg>0 then
-		Duel.SendtoGrave(tg,REASON_EFFECT) -- Send it to the GY
-	end
 end
 
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
