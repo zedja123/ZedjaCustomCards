dab5e8618eeadd270972c0ae603fe2a192b13c91
zedja123 <miguel.zedja@hotmail.com>
2024-10-13 16:40:40 -0300

    commit

---
+++ script/c270000501.lua
@@ -0,0 +1,23 @@
+--Milacresy Captainillunism - Serenayi
+function c270000501.initial_effect(c)
+	local e1 = Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id, 0))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(s.spcon)
+	e1:SetCountLimit(1, {id, 1})
+	c:RegisterEffect(e1)
+
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0
+		or Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+
+function s.spfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0xf16)
+end
