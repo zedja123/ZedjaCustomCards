e2c1f274acab29a2c25dd581192b6d271d3e2231
zedja123 <miguel.zedja@hotmail.com>
2024-10-15 12:41:22 -0300

    COMMIT

--- script/c270000501.lua
+++ script/c270000501.lua
@@ -23,6 +23,21 @@ function c270000501.initial_effect(c)
 	local e3=e2:Clone()
 	e3:SetCode(EFFECT_UPDATE_DEFENSE)
 	c:RegisterEffect(e3)
+	-- Add 1 "Milacresy" card from GY or banished to hand
+	local e4=Effect.CreateEffect(c)
+	e4:SetCategory(CATEGORY_TOHAND)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCode(EVENT_SUMMON_SUCCESS)
+	e4:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e4:SetCountLimit(1, {id, 2})
+	e4:SetTarget(s.thtg)
+	e4:SetOperation(s.thop)
+	c:RegisterEffect(e4)
+
+	-- Special Summon trigger
+	local e5=e4:Clone()
+	e5:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e5)
 end
 function s.spcon(e,c)
 	if c==nil then return true end
@@ -44,4 +59,25 @@ end
 -- Target: Apply the effect to all "Milacresy" monsters (set code 0xf16)
 function s.atktg(e,c)
 	return c:IsSetCard(0xf16)
+end
+
+-- Target 1 "Milacresy" card in GY or banished; add it to your hand
+function s.thfilter(c)
+	return c:IsSetCard(0xf16) and (c:IsLocation(LOCATION_GRAVE) or c:Islocation(LOCATION_REMOVED)) and c:IsAbleToHand()
+end
+
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and s.thfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.thfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectTarget(tp,s.thfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+
+-- Add to hand operation
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+	end
 end
