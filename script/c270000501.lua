e67d2defcfaa330b5dd67a8be78b2aeb86de8a43
zedja123 <miguel.zedja@hotmail.com>
2024-10-15 12:24:41 -0300

    Update c270000501.lua

--- script/c270000501.lua
+++ script/c270000501.lua
@@ -2,7 +2,6 @@
 local s,id=GetID()
 function c270000501.initial_effect(c)
 	local e1 = Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id, 0))
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_SPSUMMON_PROC)
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
@@ -10,7 +9,14 @@ function c270000501.initial_effect(c)
 	e1:SetCondition(s.spcon)
 	e1:SetCountLimit(1, {id, 1})
 	c:RegisterEffect(e1)
-
+	-- ATK/DEF boost during the Battle Phase
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_BATTLE_START)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCondition(s.atkcon)
+	e2:SetOperation(s.atkop)
+	c:RegisterEffect(e2)
 end
 function s.spcon(e,c)
 	if c==nil then return true end
@@ -21,4 +27,29 @@ end
 
 function s.spfilter(c)
 	return c:IsFaceup() and c:IsSetCard(0xf16)
+end
+
+-- Condition: During the Battle Phase and control a "Milacresy" monster (0xf16)
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetAttacker()
+	return tc and tc:IsSetCard(0xf16) and tc:IsControler(tp)
+end
+
+-- ATK/DEF Boost Operation: +300 ATK/DEF to all "Milacresy" monsters
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsSetCard,tp,LOCATION_MZONE,0,nil,0xf16)
+	for tc in aux.Next(g) do
+		-- ATK boost
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(300)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE_CAL)
+		tc:RegisterEffect(e1)
+
+		-- DEF boost
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_UPDATE_DEFENSE)
+		tc:RegisterEffect(e2)
+	end
 end
