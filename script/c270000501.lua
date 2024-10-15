06b825a6930df8954cb48afc6d3243296eb74129
zedja123 <miguel.zedja@hotmail.com>
2024-10-15 12:28:03 -0300

    Update c270000501.lua

--- script/c270000501.lua
+++ script/c270000501.lua
@@ -9,14 +9,20 @@ function c270000501.initial_effect(c)
 	e1:SetCondition(s.spcon)
 	e1:SetCountLimit(1, {id, 1})
 	c:RegisterEffect(e1)
-	-- ATK/DEF boost during the Battle Phase
+	-- ATK/DEF boost during the entire Battle Phase
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
 	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
 	e2:SetCondition(s.atkcon)
-	e2:SetOperation(s.atkop)
+	e2:SetTarget(s.atktg)
+	e2:SetValue(300)
 	c:RegisterEffect(e2)
+	
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_UPDATE_DEFENSE)
+	c:RegisterEffect(e3)
 end
 function s.spcon(e,c)
 	if c==nil then return true end
@@ -29,27 +35,13 @@ function s.spfilter(c)
 	return c:IsFaceup() and c:IsSetCard(0xf16)
 end
 
--- Condition: During the Battle Phase and control a "Milacresy" monster (0xf16)
-function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetAttacker()
-	return tc and tc:IsSetCard(0xf16) and tc:IsControler(tp)
+
+-- Condition: Battle Phase and you control a "Milacresy" monster
+function s.atkcon(e)
+	return Duel.IsBattlePhase()
 end
 
--- ATK/DEF Boost Operation: +300 ATK/DEF to all "Milacresy" monsters
-function s.atkop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(Card.IsSetCard,tp,LOCATION_MZONE,0,nil,0xf16)
-	for tc in aux.Next(g) do
-		-- ATK boost
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetValue(300)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE_CAL)
-		tc:RegisterEffect(e1)
-
-		-- DEF boost
-		local e2=e1:Clone()
-		e2:SetCode(EFFECT_UPDATE_DEFENSE)
-		tc:RegisterEffect(e2)
-	end
+-- Target: Apply the effect to all "Milacresy" monsters (set code 0xf16)
+function s.atktg(e,c)
+	return c:IsSetCard(0xf16)
 end
