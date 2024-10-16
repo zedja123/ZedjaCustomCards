39aa53a3c614e408fb6243c73e96be7583bc8a64
zedja123 <miguel.zedja@hotmail.com>
2024-10-16 20:11:47 -0300

    Update c270000502.lua

--- script/c270000502.lua
+++ script/c270000502.lua
@@ -72,5 +72,5 @@ function s.banop(e,tp,eg,ep,ev,re,r,rp)
 end
 -- Filter for "Milacresy" monster Special Summon
 function s.spfilter(c,e,tp)
-	return c:IsSetCard(0xf16) and c:IsCanBeSpecialSummoned(c,0,tp,false,false) and not c:IsCode(id)
+	return c:IsSetCard(0xf16) and not c:IsCode(id)
 end
