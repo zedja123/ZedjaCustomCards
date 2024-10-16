98405aa169c427d1788519a8b6cc276bb1406633
zedja123 <miguel.zedja@hotmail.com>
2024-10-16 19:40:53 -0300

    Update c270000502.lua

--- script/c270000502.lua
+++ script/c270000502.lua
@@ -1,68 +1,36 @@
 local s,id=GetID()
 function s.initial_effect(c)
-	-- Special Summon from GY by sending itself and 1 other card from hand to GY
+	-- Special Summon from GY by sending this card and 1 other card from hand to the GY
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOGRAVE)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_HANDES)
 	e1:SetType(EFFECT_TYPE_IGNITION)
-	e1:SetRange(LOCATION_HAND+LOCATION_GRAVE)
+	e1:SetRange(LOCATION_GRAVE)
 	e1:SetCountLimit(1,id)
 	e1:SetCost(s.spcost)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
-
-	-- Banish 3 cards from Deck and Special Summon Milacresy monster from GY or Banished
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_REMOVE+CATEGORY_SPECIAL_SUMMON)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetCode(EVENT_SUMMON_SUCCESS)
-	e2:SetCountLimit(1,{id,1})
-	e2:SetTarget(s.sptg2)
-	e2:SetOperation(s.spop2)
-	c:RegisterEffect(e2)
-	local e3=e2:Clone()
-	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
-	c:RegisterEffect(e3)
 end
 
--- Cost for sending this card and another card from hand to GY
+-- Cost: Send this card and 1 other card from hand to GY
 function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,e:GetHandler()) end
 	Duel.SendtoGrave(e:GetHandler(),REASON_COST)
 	Duel.DiscardHand(tp,nil,1,1,REASON_COST+REASON_DISCARD)
 end
 
--- Target for Special Summon
+-- Target: Special Summon this card from GY
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
 
--- Special Summon from GY
+-- Operation: Special Summon this card from GY
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
-		-- Optional additional actions upon successful Special Summon
-	end
-end
-
--- Target for banishing 3 cards and special summon Milacresy monster from banished or GY
-function s.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsSetCard,0xf16),tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil)
-		and Duel.IsPlayerCanRemove(tp) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
-	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,3,tp,LOCATION_DECK)
-end
-
--- Banish 3 and Special Summon
-function s.spop2(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.Remove(Duel.GetDecktopGroup(tp,3),POS_FACEUP,REASON_EFFECT)>0 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(aux.FilterFaceupFunction(Card.IsSetCard,0xf16)),tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,1,nil)
-		if #g>0 then
-			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
-		end
+		-- Optional additional effects
 	end
 end
