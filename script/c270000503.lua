643ce31be70e3a8014c18e684e953ef40f97a8a0
zedja123 <miguel.zedja@hotmail.com>
2024-10-16 20:46:34 -0300

    Create c270000503.lua

---
+++ script/c270000503.lua
@@ -0,0 +1,62 @@
+--Milacresy Extrallunism - Bibi-bee
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Special Summon from GY or banished if sent by a "Milacresy" card effect
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TODECK)
+	e1:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
+	e1:SetCode(EVENT_TO_GRAVE+EVENT_REMOVE)
+	e1:SetCondition(s.spcon)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	e1:SetCountLimit(1,id)
+	c:RegisterEffect(e1)
+
+	-- Add a "Milacresy" Spell/Trap when Special Summoned
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetCountLimit(1,id+100)
+	e2:SetTarget(s.addtg)
+	e2:SetOperation(s.addop)
+	c:RegisterEffect(e2)
+end
+
+-- Condition: Check if sent to GY by a "Milacresy" card effect
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return re and re:IsActiveType(TYPE_SPELL+TYPE_TRAP) and re:IsSetCard(0xf16)
+end
+
+-- Target: Select 3 "Milacresy" cards from the banished or GY
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.NecroValleyFilter(Card.IsSetCard), tp, LOCATION_GRAVE+LOCATION_REMOVED, 0, 3, nil, 0xf16) end
+	Duel.SetOperationInfo(0, CATEGORY_TODECK, nil, 0, tp, 3)
+end
+
+-- Operation: Shuffle and Special Summon this card
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(aux.NecroValleyFilter(Card.IsSetCard), tp, LOCATION_GRAVE+LOCATION_REMOVED, 0, nil, 0xf16)
+	if #g>0 and Duel.SendtoDeck(g,nil,2,REASON_EFFECT)~=0 then
+		Duel.BreakEffect()
+		Duel.SpecialSummon(e:GetHandler(),0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+
+-- Target: Add a "Milacresy" Spell/Trap from Deck to hand
+function s.addtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsSetCard,tp,LOCATION_DECK,0,1,nil,0xf16) end
+	Duel.SetOperationInfo(0, CATEGORY_TOHAND, nil, 0, tp, 1)
+end
+
+-- Operation: Add the selected Spell/Trap to hand
+function s.addop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,Card.IsSetCard,tp,LOCATION_DECK,0,1,1,nil,0xf16)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
