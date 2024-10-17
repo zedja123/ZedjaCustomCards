29e71f8a571ec759aa19a473ac9c944815eb3f10
zedja123 <miguel.zedja@hotmail.com>
2024-10-17 00:18:30 -0300

    Update c270000503.lua

--- script/c270000503.lua
+++ script/c270000503.lua
@@ -29,13 +29,13 @@ function s.initial_effect(c)
 end
 
 function s.shfilter(c)
-	return c:IsSetCard(0xf16) and c:IsAbleToDeckOrExtraAsCost() and not c:IsCode(id)
+	return c:IsSetCard(0xf16) and c:IsAbleToDeckOrExtraAsCost()
 end
 
 -- Cost: Shuffle 3 "Milacresy" cards from your GY or banished
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.shfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,3,nil) end
-	local g=Duel.SelectMatchingCard(tp,s.shfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,3,3,nil)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.shfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,3,c) end
+	local g=Duel.SelectMatchingCard(tp,s.shfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,3,3,c)
 	Duel.HintSelection(g)
 	Duel.SendtoDeck(g,nil,3,REASON_COST)
 end
