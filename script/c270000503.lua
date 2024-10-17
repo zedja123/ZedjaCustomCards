35628a14ddee76ddc448eb0b8a8cc35b1956ffe1
zedja123 <miguel.zedja@hotmail.com>
2024-10-16 23:53:14 -0300

    Update c270000503.lua

--- script/c270000503.lua
+++ script/c270000503.lua
@@ -34,8 +34,8 @@ end
 
 -- Cost: Shuffle 3 "Milacresy" cards from your GY or banished
 function s.cost(c,e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,3,c) end
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,3,3,c)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.shfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,3,c) end
+	local g=Duel.SelectMatchingCard(tp,s.shfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,3,3,nil,c)
 	Duel.HintSelection(g)
 	Duel.SendtoDeck(g,nil,3,REASON_COST)
 end
