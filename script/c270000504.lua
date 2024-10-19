fa86db3b57b0b284233d9c38feb132afed5378f7
zedja123 <miguel.zedja@hotmail.com>
2024-10-19 20:30:20 -0300

    Update c270000504.lua

--- script/c270000504.lua
+++ script/c270000504.lua
@@ -16,9 +16,12 @@ local e2=e1:Clone()
 e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 c:RegisterEffect(e2)
 local e3=Effect.CreateEffect(c)
+e3:SetCategory(CATEGORY_DISABLE)
 e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_QUICK_O)
-e3:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
---e3:SetCondition(s.cond)
+e3:SetCode(EVENT_FREE_CHAIN)
+e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_CARD_TARGET)
+e3:SetRange(LOCATION_MZONE)
+e3:SetCondition(s.cond)
 e3:SetCountLimit(1,{id,2})
 e3:SetTarget(s.matlimit_target)
 e3:SetOperation(s.matlimit_operation)
@@ -29,9 +32,10 @@ function s.cond(c)
 	return Duel.IsMainPhase()
 end
 
-function s.matlimit_target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return end
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingTarget(Card.IsOnField,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local g=Duel.SelectTarget(tp,Card.IsOnField,tp,0,LOCATION_MZONE,1,1,nil)
 end
 
 function s.matlimit_operation(e,tp,eg,ep,ev,re,r,rp)
