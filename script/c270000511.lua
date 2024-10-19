76ea5d29408f17e72fccc3191337bc3dc3acb31c
zedja123 <miguel.zedja@hotmail.com>
2024-10-19 11:39:45 -0300

    Update c270000511.lua

--- script/c270000511.lua
+++ script/c270000511.lua
@@ -2,7 +2,7 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	-- Synchro Summon
-	Synchro.AddProcedure(c,s.tunerfilter,1,1,s.nontunerfilter,1,1) -- "Milacresy" Tuner and non-Tuner
+	Synchro.AddProcedure(c,s.tunerfilter,1,1,s.nontunerfilter,1,1,Card.IsLinkMonster) -- "Milacresy" Tuner and non-Tuner
 	c:EnableReviveLimit()
 	-- Effect: Banish 5 cards and Special Summon
 	local e1=Effect.CreateEffect(c)
