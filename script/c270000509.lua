019042722338c8655b20643657b4c3c8f31c9741
zedja123 <miguel.zedja@hotmail.com>
2024-10-19 08:22:04 -0300

    Create c270000509.lua

---
+++ script/c270000509.lua
@@ -0,0 +1,36 @@
+-- Milacresy Sacrificorius
+function s.initial_effect(c)
+	-- Link Summon
+	aux.AddLinkProcedure(c,s.matfilter,2,2)
+	c:EnableReviveLimit()
+
+	-- Effect: Send 1 "Milacresy" card and add a banished or GY card
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SENDTOGRAVE+CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCountLimit(1)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+
+function s.matfilter(c)
+	return c:IsSetCard(0xf16) -- Filter for "Milacresy" monsters
+end
+
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsSetCard,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,nil,0xf16) end
+end
+
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=Duel.SelectMatchingCard(tp,Card.IsSetCard,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,1,nil,0xf16)
+	if #g>0 and Duel.SendtoGrave(g,REASON_EFFECT)~=0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg=Duel.SelectMatchingCard(tp,aux.FaceupFilter(Card.IsSetCard,0xf16),tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,1,g)
+		if #sg>0 then
+			Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		end
+	end
+end
