ce117d3cc9ce6cbba84c52a2854a13757cdd9786
zedja123 <miguel.zedja@hotmail.com>
2024-10-26 05:26:49 -0300

    Update c270000414.lua

--- script/c270000414.lua
+++ script/c270000414.lua
@@ -38,7 +38,7 @@ function s.initial_effect(c)
 
 	-- Gain 1000 ATK if a card is banished
 	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e4:SetCode(EVENT_REMOVE)
 	e4:SetRange(LOCATION_MZONE)
 	e4:SetCondition(s.atkcon)
@@ -79,12 +79,7 @@ end
 
 -- Condition: If any card is banished
 function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.cfilter,1,nil)
-end
-
--- Filter: Check if a card is banished
-function s.cfilter(c)
-	return not c:IsPreviousLocation(LOCATION_REMOVED)
+	return #eg>0
 end
 
 -- Operation: Gain 1000 ATK for the rest of this turn
