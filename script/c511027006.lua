be420c7458ae398598ca155a9b86dda8b38fcb12
zedja123 <miguel.zedja@hotmail.com>
2025-02-12 12:46:59 -0300

    commit

--- script/c511027006.lua
+++ script/c511027006.lua
@@ -46,8 +46,8 @@ end
 
 -- (2) Special Summon if you control a Link-1 monster
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsType,TYPE_LINK),tp,LOCATION_MZONE,0,1,nil) and
-		   Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsLink,1),tp,LOCATION_MZONE,0,1,nil)
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsType,TYPE_LINK),tp,LOCATION_MZONE,0,1,nil) and
+		   Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsLink,1),tp,LOCATION_MZONE,0,1,nil)
 end
 function s.spfilter(c,e,tp)
 	return c:IsSetCard(0x700) and c:IsLevel(1) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
