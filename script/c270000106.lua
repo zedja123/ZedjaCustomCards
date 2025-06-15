ee369f620f2b270190fdbc79e084c6940c863a00
zedja123 <miguel.zedja@hotmail.com>
2025-06-15 06:19:51 -0300

    Update c270000106.lua

--- script/c270000106.lua
+++ script/c270000106.lua
@@ -30,34 +30,36 @@ function s.filter(c,e)
 end
 
 function s.validGroup(g,tp)
-	local n=#g
-	if n==1 then
-		local c=g:GetFirst()
-		return c:IsType(TYPE_XYZ) and c:IsSetCard(0xf11) and c:GetRank()<=4
-			and Duel.IsExistingMatchingCard(s.xyzfilter,tp,LOCATION_EXTRA,0,1,nil,g,tp)
-	elseif n==2 then
+	local n = #g
+	if n == 1 then
+		local c = g:GetFirst()
+		return c:IsType(TYPE_XYZ) and c:IsSetCard(0xf11) and c:GetRank() <= 4
+			and Duel.IsExistingMatchingCard(s.xyzfilter, tp, LOCATION_EXTRA, 0, 1, nil, g, tp)
+	elseif n == 2 then
 		for tc in aux.Next(g) do
-			if tc:IsType(TYPE_XYZ) then return false end
+			if tc:IsType(TYPE_XYZ) then return false end -- Cannot pair with Xyz
 		end
-		return Duel.IsExistingMatchingCard(s.xyzfilter,tp,LOCATION_EXTRA,0,1,nil,g,tp)
+		return Duel.IsExistingMatchingCard(s.xyzfilter, tp, LOCATION_EXTRA, 0, 1, nil, g, tp)
 	end
 	return false
 end
 
 function s.xyzfilter(c,mg,tp)
-	return c:IsSetCard(0xf11) and c:IsXyzSummonable(nil,mg,1,2) and Duel.GetLocationCountFromEx(tp,tp,mg,c)>0
+	return c:IsSetCard(0xf11) and c:IsXyzSummonable(nil,mg,1,2)
+		and Duel.GetLocationCountFromEx(tp,tp,mg,c) > 0
 end
 
--- Target: only pick 1 monster, always
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	local mg=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,0,nil,e)
+	if chkc then return false end
+	local mg = Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,0,nil,e)
 	if chk==0 then
-		for tc in mg:Iter() do
-			local g1=Group.FromCards(tc)
+		-- Check for at least one valid solo or pair
+		for tc1 in mg:Iter() do
+			local g1 = Group.FromCards(tc1)
 			if s.validGroup(g1,tp) then return true end
-			for other in mg:Iter() do
-				if other~=tc then
-					local g2=Group.FromCards(tc,other)
+			for tc2 in mg:Iter() do
+				if tc1 ~= tc2 then
+					local g2 = Group.FromCards(tc1, tc2)
 					if s.validGroup(g2,tp) then return true end
 				end
 			end
@@ -65,44 +67,82 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 		return false
 	end
 
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
-	local first=mg:Select(tp,1,1,nil):GetFirst()
-	Duel.SetTargetCard(first)
-	e:SetLabelObject(first)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
-end
-
--- Activate: determine whether to resolve or request a second pick
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local first=e:GetLabelObject()
-	if not first or not first:IsRelateToEffect(e) or not first:IsFaceup() then return end
-
-	local g1=Group.FromCards(first)
-	if s.validGroup(g1,tp) then
-		-- Valid solo: proceed
-		s.xyzSummon(tp, g1)
-		return
-	end
-
-	-- Must pick a valid pair
-	local mg=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,0,nil,e)
-	local secondPool=Group.CreateGroup()
-	for tc in mg:Iter() do
-		if tc~=first then
-			local pair=Group.FromCards(first,tc)
-			if s.validGroup(pair,tp) then
-				secondPool:AddCard(tc)
+	-- Build first-pickable group
+	local selectable = Group.CreateGroup()
+	for tc1 in mg:Iter() do
+		local g1 = Group.FromCards(tc1)
+		if s.validGroup(g1,tp) then
+			selectable:AddCard(tc1)
+		else
+			for tc2 in mg:Iter() do
+				if tc1 ~= tc2 then
+					local g2 = Group.FromCards(tc1, tc2)
+					if s.validGroup(g2,tp) then
+						selectable:AddCard(tc1)
+						break
+					end
+				end
 			end
 		end
 	end
-	if #secondPool==0 then return end
+
+	if #selectable == 0 then return end
 
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
-	local second=secondPool:Select(tp,1,1,nil):GetFirst()
-	if not second then return end
+	local firstGroup = selectable:Select(tp,1,1,nil)
+	if not firstGroup or #firstGroup==0 then return end
+	local first = firstGroup:GetFirst()
+	local g1 = Group.FromCards(first)
 
-	local finalGroup=Group.FromCards(first,second)
-	s.xyzSummon(tp, finalGroup)
+	if s.validGroup(g1,tp) then
+		-- Solo valid
+		Duel.SetTargetCard(g1)
+		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+		return
+	end
+
+	-- Find valid second partners
+	local secondables = Group.CreateGroup()
+	for tc in mg:Iter() do
+		if tc ~= first then
+			local pair = Group.FromCards(first, tc)
+			if s.validGroup(pair,tp) then
+				secondables:AddCard(tc)
+			end
+		end
+	end
+
+	if #secondables == 0 then return end -- shouldn't happen due to earlier filtering
+
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
+	local secondGroup = secondables:Select(tp,1,1,nil)
+	if not secondGroup or #secondGroup==0 then return end
+
+	local finalGroup = Group.FromCards(first)
+	finalGroup:Merge(secondGroup)
+	Duel.SetTargetCard(finalGroup)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g = Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(s.tfilter,nil,e)
+	if #g == 0 then return end
+
+	local xyzg = Duel.GetMatchingGroup(s.xyzfilter, tp, LOCATION_EXTRA, 0, nil, g, tp)
+	if #xyzg == 0 then return end
+
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local sel = xyzg:Select(tp,1,1,nil)
+	if #sel == 0 then return end
+
+	local xyz = sel:GetFirst()
+	if xyz then
+		Duel.XyzSummon(tp, xyz, nil, g)
+	end
+end
+
+function s.tfilter(c,e)
+	return c:IsRelateToEffect(e) and c:IsFaceup()
 end
 
 -- Xyz Summon helper
